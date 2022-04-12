bba93388b67371b12fd18e0701097ac8052552a3
brante004 <sftan@uchicago.edu>
2022-04-12 12:02:55 -0500

    Update lab - lecture 5.py
    Finished q1 and q2 today

--- lab - lecture 5.py
+++ lab - lecture 5.py
@@ -5,14 +5,14 @@ a = 10
 
 def first_func(b=20):
     c = 30
+    def second_func(d=40):
+        e=50
+        return a + b + c + d +e
     value = second_func()
     return value
 
-def second_func(d=40):
-    e = 50
-    return a + b + c + d + e
-
 result = first_func()
+print(result)
 
 #2: take this code from Thursday's lab and write a function so that the form of
 #the final answer is:
@@ -22,6 +22,19 @@ import datetime
 start_dict = {'noah': '2/23/1999',
               'sarah':'9/1/2001',
               'zach': '8/8/2005'}
+
+def key_func(k):
+    new_k = k.capitalize()
+    return new_k
+
+def val_func(v):
+    new_v = datetime.datetime.strptime(v, "%m/%d/%Y").date()
+    return new_v
+
+answer = {key_func(k):val_func(v) for k, v in start_dict.items()}
+
+print(answer)
+
 #turn it into {'Noah': datetime(1999, 2, 23),
 #              'Sarah':datetime(2001, 9, 1),
 #              'Zach': datetime(2005, 8, 8)}
